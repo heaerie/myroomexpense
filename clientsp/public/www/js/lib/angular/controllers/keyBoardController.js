1b90ac715c3d1c72a6b4d82958a6d9d18054f0bf
Duraimurugan Govindaraj <durai145@live.in>
2016-03-06 22:07:57 -0500

    webmail link added

--- clientsp/public/www/js/lib/angular/controllers/keyBoardController.js
+++ clientsp/public/www/js/lib/angular/controllers/keyBoardController.js
@@ -1438,6 +1438,86 @@ $scope.$watch('$viewContentLoaded', function(){
     //$scope.getUserDetail();
   });
 
+USSDoc_=function()
+{
+
+this.group      = 'USS', /*it has been chabged by durai on 02-Feb-2010*/
+this.name       = '',
+this.label      = '',
+this.task       = 'NONE',
+this.title       = 'NONE',
+this.documentText       = 'NONE',
+this.documentHTML       = '<div>this is test</div>',
+this.desc       = '',
+this.htmlType   = 'text', /* newly introduced in USS05*/
+this.entitle    = 'READONLY'; // Editable /Readonly
+this.enttlname  = ''; //Entitle name to db
+this.mndf       = 'N',
+this.dataType   = 'VARCHAR',  // NUMBER/VARCHAR/DATE/EMAIL/AMOUNT/LIST/DIV/
+this.cclass     = 'ctable',   //
+this.parent     = '',
+this.parentHtmlType     = '',
+this.validate   = '',
+this.dflt       = '',
+this.min        = '0',
+this.max        = '60',
+this.tips       = '',
+this.onkeyup    = 'onKeyUp(this);',
+this.onchange   = 'onChange(this);',
+this.onkeydown  = 'onKeyDown(this);',
+this.onkeypress = 'onKeyPress(this);',
+this.onclick    = 'onClick(this);',
+this.onblure    = 'onBlure(this);',
+this.listVal    = '||A|A-ADD|M|M-MODIFY|I|I-INQURY|C|C-CANCEL|V|V-VERIFY',
+this.help       = 'N',
+this.helpLink   = 'helpload',
+this.xml        = 'Y',
+this.xmlname    = '',
+this.Xpath      = '/' ,
+this.maxCol     = '1',
+this.col        = '0'
+  
+}
+
+$scope.createTreeNode= function(treeNode)
+{
+
+}
+
+
+   $scope.createTreeHeader=function(fieldObj)
+  {
+      this.elm= document.createElement("div");
+      this.elm.appendChild(document.createTextNode(fieldObj.title));
+      return this.elm;
+  }
+   $scope.createTreeBody=function(fieldObj)
+  {
+    this.elm= document.createElement("div");
+      this.elm.appendChild(document.createTextNode(fieldObj.documentText));
+      var ed=document.getElementById('editor').innerHTML;
+      //var editor=document.getElementById("editor");
+    //   alert(ed);
+
+       document.getElementById('editor').innerHTML=fieldObj.documentHTML;
+      return this.elm;
+
+  }
+
+   
+
+   $scope.viewDoc=function()
+   {
+     var docObj=new USSDoc_()
+     docObj.documentHTML=document.getElementById('editor').innerHTML;
+     docObj.documentText=document.getElementById('editor').innerText;
+
+   $scope.createTreeBody(docObj);
+   }
+
+
+
+
 
       // alert("basicDetService");
 	
